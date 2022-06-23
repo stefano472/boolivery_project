1b47917842012ca165e67d5aa0fdb9015971c9ff
stefano472 <stefanopiotti472@gmail.com>
2022-06-23 17:01:25 +0200

    first migration main tables

---
+++ database/migrations/2022_06_23_144459_create_restaurants_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRestaurantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('restaurants', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name', 30);
+            $table->string('address', 255);
+            $table->string('motto', 255);
+            $table->string('tax_id', 15);
+            $table->tinyInteger('phone_number');
+            $table->string('cover');
+            $table->string('logo');
+            $table->mediumText('description');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('restaurants');
+    }
+}
