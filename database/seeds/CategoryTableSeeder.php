6d4da5d221234d0369abf718b91c19cfccf391bf
Davide Pulci <davide.pulci@gmail.com>
2022-06-25 10:47:59 +0200

    add category seeder

---
+++ database/seeds/CategoryTableSeeder.php
@@ -0,0 +1,70 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CategoryTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $categories=[
+            [
+                'name'=>'italiano',
+                'img'=>''
+            ],
+            [
+                'name'=>'giapponese',
+                'img'=>''
+            ],
+            [
+                'name'=>'cinese',
+                'img'=>''
+            ],
+            [
+                'name'=>'messicano',
+                'img'=>''
+            ],
+            [
+                'name'=>'fast-food',
+                'img'=>''
+            ],
+            [
+                'name'=>'pizza',
+                'img'=>''
+            ],
+            [
+                'name'=>'vegano',
+                'img'=>''
+            ],
+            [
+                'name'=>'dolci',
+                'img'=>''
+            ],
+            [
+                'name'=>'tailandese',
+                'img'=>''
+            ],
+            [
+                'name'=>'indiano',
+                'img'=>''
+            ],
+            [
+                'name'=>'kebab',
+                'img'=>''
+            ],
+            [
+                'name'=>'poke',
+                'img'=>''
+            ],
+            [
+                'name'=>'street-food',
+                'img'=>''
+            ]
+        ];
+    }
+}
