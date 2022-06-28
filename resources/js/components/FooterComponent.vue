1387d86da2d5862bce5ab2dc367e5a24d085f546
Alessandro Giunta <alegiunt99@gmail.com>
2022-06-28 15:57:22 +0100

    Social Component + FooterComponent completed

--- resources/js/components/FooterComponent.vue
+++ resources/js/components/FooterComponent.vue
@@ -1,7 +1,97 @@
 <template>
-  <div>
-    footer
-  </div>
+  <footer>
+    <div class="container-footer">
+
+      <!-- TITLE -->
+      <div class="footer-title">
+        <h5>Hai bispgno di aiuto? Contattaci!</h5>
+      </div>
+
+
+      <!-- CONTENT -->
+      <div class="footer-content">
+        
+        <!-- logo and site language -->
+        <div class="logo-lang">
+          
+          <div class="logo">
+            <img src="../../../public/logo.svg" alt="">
+          </div>
+
+          <div class="language">
+            <i class="fa-solid fa-globe"></i>
+            <span>Italiano </span>
+          </div>
+
+        </div>
+
+        <!-- contacts -->
+        <div class="contact-us">
+          <!-- contact us title -->
+          <p id="contact-title">Contatti</p>
+
+          <div class="contacts">
+
+            <!-- PHONE NUMBER -->
+            <div class="ico-text">
+
+              <!-- icon contacts -->
+              <i class="fa-solid fa-phone-flip"></i>
+
+              <!-- text contacts -->
+              <p>+39 3823332389</p>
+
+            </div>
+
+            <!-- FAX -->
+            <div class="ico-text">
+
+              <!-- icon contacts -->
+              <i class="fa-solid fa-fax"></i>
+
+              <!-- text contacts -->
+              <p>390639967700@efaxsend.com</p>
+
+            </div>
+
+            <!-- EMAIL ADDRESS -->
+            <div class="ico-text">
+              
+              <!-- icon contacts -->
+              <i class="fa-solid fa-envelope"></i>
+
+              <!-- text contacts -->
+              <p>boolivery@contacts.com</p>
+
+            </div>
+
+
+          </div>
+        </div>
+
+        <!-- instagram feed -->
+        <div class="instagram-feed">
+          
+          <!-- INSTA FEED TITLE -->
+          <p>Instagram feed</p>
+
+          <!-- PHOTOS -->
+          <div class="insta-photos">
+            <img src="../../../public/images/insta-feed.jpg" alt="">
+          </div>
+
+        </div>
+
+      </div>
+
+      <!-- COPYRIGHT -->
+      <div class="copyright">
+        <p>&copy; 2022 Powered by Team 1</p>
+      </div>
+
+
+    </div>
+  </footer>
 </template>
 
 <script>
@@ -10,6 +100,188 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+
+footer{
+
+  background-color: #1e282f;
+  .container-footer{
+    width: 80%;
+    margin: 0 auto;
+    padding: 28px 0;
+    color: #38a3a5;
+    .footer-title{
+      text-align: center;
+    }
+    .footer-content{
+      display: flex;
+      justify-content: space-between;
+      margin: 34px 0;
+      align-items: center;
+      flex-wrap: wrap;
+
+      .logo-lang{
+        width: 20%;
+        text-align: center;
+        padding-top: 28px;
+        img{
+          width: 87px;
+          margin-bottom: 22px;
+        }
+        .language{
+          font-size: 12px;
+          color: gray;
+        }
+      }
+
+      .contact-us{
+        display: flex;
+        flex-direction: column;
+        padding-left: 8%;
+        width: 40%;
+
+        #contact-title{
+          font-size: 20px;
+          font-weight: 600;
+          color: #38a3a5;
+        }
+        .contacts{
+          row-gap: 5px;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          margin-top: 7px;
+
+          .ico-text {
+            display: flex;
+            column-gap: 18px;
+            align-items: center;
+            margin-bottom: 20px;
+            padding-right: 40px;
+            i{
+              color: #98f1ad;
+            }
+            p{
+              margin-bottom: 0;
+              color: white;
+            }
+          }
+
+        }
+      }
+
+      .instagram-feed{
+        display: flex;
+        flex-direction: column;
+        width: 20%;
+        p{
+          font-size: 20px;
+          font-weight: 600;
+          color: #38a3a5;
+        }
+        img{
+          width: 180px;
+
+        }
+      }
+    }
+    .copyright{
+      text-align: center;
+      padding-top: 30px;
+      border-top: 1px solid gray;
+    }
+
+  }
+}
+
+
+ @media screen and (max-width: 850px){
+
+    footer{
+      .container-footer{
+        .contact-us{
+          padding-left: 4%;
+        }
+      }
+    }
+        
+ }
+
+
+@media screen and (max-width: 770px){
+
+    footer{
+      .container-footer{
+
+        .footer-content{
+          justify-content: space-around;
+          row-gap: 41px;
+
+          .logo-lang{
+            width: 42%;
+          }
+          .contact-us{
+            width: 42%;
+            padding-left: 0%;
+          }
+
+          .instagram-feed{
+            width: 40%;
+            align-items: center;
+          }
+        }
+        
+      }
+    }
+        
+ }
+
+ @media screen and (max-width: 593px){
+
+    footer{
+      .container-footer{
+
+        .footer-content{
+          justify-content: space-around;
+          row-gap: 41px;
+
+          .logo-lang{
+            width: 100%;
+          }
+          .contact-us{
+            width: 100%;
+            padding-left: 0%;
+            align-items: center;
+          }
+
+          .instagram-feed{
+            width: 100%;
+            align-items: center;
+          }
+        }
+        
+      }
+    }
+        
+ }
+
+  @media screen and (max-width: 326px){
+
+    footer{
+      .container-footer{
+        .footer-content{
+          .contact-us{
+            .contacts{
+              font-size: 12px;
+              padding-left: 22%;
+            }
+          }
+        }
+        
+      }
+    }
+        
+ }
+
 
 </style>
---
+++ resources/js/components/home/SocialComponent.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="social-component">
+    <!-- Titolo dei social -->
+    <h1>SEGUICI SUI SOCIAL</h1>
+
+    <!-- icone dei social -->
+    <div class="social-box">
+
+        <!-- Facebook -->
+        <div class="social-icon">
+            <a href="https://www.facebook.com/" target="_blank">
+                <i class="fa-brands fa-facebook"></i>
+            </a>
+        </div>
+
+        <!-- Instagram -->
+        <div class="social-icon">
+            <a href="https://www.instagram.com/" target="_blank">
+                <i class="fa-brands fa-instagram"></i>
+            </a>
+        </div>
+        
+
+        <!-- Twitter -->
+        <div class="social-icon">
+            <a href="https://twitter.com/" target="_blank">
+                <i class="fa-brands fa-twitter"></i>
+            </a>
+        </div>
+
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'SocialComponent',
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+    .social-component{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        row-gap: 27px;
+        padding: 77px 0;
+        background-color: #38a3a5;
+        color: white;
+        .social-box{
+            display: flex;
+            width: 275px;
+            justify-content: space-between;
+            .social-icon{
+                a{
+                    color: black;
+                    font-size: 30px;
+                    background-color: #98f1ad;
+                    border-radius: 50%;
+                    padding-top: 16px;
+                    padding-left: 14px;
+                    padding-right: 14px;
+                    padding-bottom: 9px;
+                    &:hover{
+                        background-color: #cafbd6;
+                    }
+                }
+            }
+        }
+}
+
+ @media screen and (max-width: 715px){
+
+    .social-component{
+        .social-box{
+            width: 42%;
+        }
+    }
+        
+ }
+
+
+ @media screen and (max-width: 459px){
+
+    .social-component{
+        .social-box{
+         flex-wrap: wrap;
+         .social-icon{
+              width: 100%;
+              text-align: center;
+                margin-bottom: 47px;
+                a{
+                    font-size: 53px;
+                }
+             }
+        }   
+    }
+        
+ }
+
+ @media screen and (max-width: 333px){
+    .social-component{
+        h1{
+            font-size: 21px;
+        }
+    }  
+ }
+</style>
