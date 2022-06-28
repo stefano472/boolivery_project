1079d29129b2f3ec1fbcd63b910ffb173e10ed63
Maria210194 <mary.stachie@gmail.com>
2022-06-28 17:26:42 +0200

    CategoriePiuRichieste + RisstorantiPiuCercati

---
+++ resources/js/components/home/CategoriePiuRichiesteComponent.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="ms-container">
+    <section class="category">
+        <div>
+            <h2 class="title-categ">
+                Le categorie piu' richieste
+            </h2>
+        </div>
+        <div class="row1 mt-5">
+            <img class="single-cat pad-1" src="/img/category_img/cat_veg.jpeg" alt="">
+            <img class="single-cat pad-2" src="/img/category_img/cat_mex.png" alt="">
+            <img class="single-cat pad-3" src="/img/category_img/cat_fast.png" alt="">
+            <img class="single-cat pad-4" src="/img/category_img/cat_piz.png" alt="">
+        </div>
+        <div class="row1">
+            <img class="single-cat pad-1" src="/img/category_img/cat_dol.png" alt="">
+            <img class="single-cat pad-2" src="/img/category_img/cat_jap.png" alt="">
+            <img class="single-cat pad-3" src="/img/category_img/cat_chi.png" alt="">
+            <img class="single-cat pad-4" src="/img/category_img/cat_ita.png" alt="">
+        </div>
+
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'CategoriePiuRichiesteComponent'
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import 'resources/sass/variables';
+
+#ms-container{
+    max-width: 1400px;
+    .category{
+        padding: 30px 0 30px;
+    }
+    .title-categ{
+        text-transform: uppercase;
+        text-align: center;
+        font-size: 1.7rem;
+        margin: 20px 0;
+    }
+    .row1{
+        display: flex;
+        justify-content: center;
+    }
+    .single-cat{
+        width: 150px;
+        height: 150px;
+        padding-right: 5px;
+
+    }
+    .pad-1{
+        margin: 0 0 40px;
+    }
+    .pad-2{
+        margin: 10px 0 30px;
+    }
+    .pad-3{
+        margin: 20px 0 20px;
+    }
+    .pad-4{
+        margin: 30px 0 10px;
+    }
+}
+
+</style>
---
+++ resources/js/components/home/RistorantiPiuCercatiComponent.vue
@@ -0,0 +1,126 @@
+<template>
+<div id="sm-container">
+    <section class="most-want">
+        <div class="main-titles">
+            <h2>I ristoranti piu' cercati</h2>
+            <h5>Selezionati proprio da voi</h5>
+        </div>
+        <div class="restaurants">
+            <div class="block">
+                <div class="add-padd">
+                    <div class="bg-img"></div>
+                </div>
+
+                <div class="box-info">
+                    <h6 class="name-restaurant">Nome ristorante</h6>
+                    <p class="description">Descrizione ristorante</p>
+                    <p class="tipology">Tipologia ristorante</p>
+                    <button class="btn-go">Vai al ristorante</button>
+                </div>
+            </div>
+            <div class="block">
+                <div class="box-info">
+                    <h6 class="name-restaurant">Nome ristorante</h6>
+                    <p class="description">Descrizione ristorante</p>
+                    <p class="tipology">Tipologia ristorante</p>
+                    <button class="btn-go">Vai al ristorante</button>
+                </div>
+                <div class="add-padd">
+                    <div class="bg-img"></div>
+                </div>
+            </div>
+            <div class="block">
+                <div class="add-padd">
+                    <div class="bg-img"></div>
+                </div>
+                <div class="box-info">
+                    <h6 class="name-restaurant">Nome ristorante</h6>
+                    <p class="description">Descrizione ristorante</p>
+                    <p class="tipology">Tipologia ristorante</p>
+                    <button class="btn-go">Vai al ristorante</button>
+                </div>
+            </div>
+        </div>
+    </section>
+
+</div>
+
+</template>
+
+<script>
+export default {
+    name: 'RistorantiPiuCercatiComponent'
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import 'resources/sass/variables';
+
+#sm-container{
+    max-width: 1400px;
+        .most-want{
+        background-color: $brand-color;
+        width: 100%;
+        min-height: 500px;
+        padding-bottom: 20px;
+        }
+        .main-titles{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding: 30px 0;
+        }
+        .block{
+            display: flex;
+            justify-content: center;
+            column-gap: 5px;
+            margin-bottom: 20px;
+        }
+        .add-padd{
+            padding: 30px;
+            width: 35%;
+            height: 250px;
+        }
+        .bg-img{
+            width: 100%;
+            height: 100%;
+            background-image: url(https://images.wallpaperscraft.com/image/single/restaurant_table_interior_modern_style_39288_1920x1080.jpg);
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-position-y: center;
+            border-radius: 5px;
+        }
+        .box-info{
+            width: 35%;
+            height: 200px;
+            display: flex;
+            flex-direction: column;
+            line-height: 1rem;
+            justify-content: center;
+            align-items: center;
+            padding-top: 50px;
+            padding-left: 1rem;
+        }
+        .name-restaurant{
+            text-transform: uppercase;
+            font-size: 1.2rem;
+        }
+        .description{
+            color: gray;
+            font-size: 0.7rem;
+        }
+        .tipology{
+            font-size: 0.7rem;
+            font-weight: 700;
+
+        }
+        .btn-go{
+            width: max-content;
+            padding: 5px;
+            background-color: transparent;
+            cursor: pointer;
+        }
+}
+
+</style>
