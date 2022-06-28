997bb969362227d1a0a3aaa7b7f7fa3c1fb97d16
Davide Pulci <davide.pulci@gmail.com>
2022-06-28 17:30:56 +0200

    CollaboraConNoiComponent + ServiziComponent

---
+++ resources/js/components/home/CollaboraConNoiComponent.vue
@@ -0,0 +1,77 @@
+<template>
+  <div id="collab">
+    <div class="collab-cover"></div>
+    <div class="collab-text">
+      <h2>COLLABORA CON NOI</h2>
+      <p>
+        Aumenta le tue vendite diventando partner del più grande servizio online
+        di ordini a domicilio d'Italia. Metti il tuo ristorante a disposizione
+        di migliaia di nuovi clienti entrando nel gruppo Boolivery. Appena
+        affiliato avrai più ordini, le tecnologie più recenti, pubblicità e un
+        servizio clienti dedicato.
+      </p>
+      <div>
+        <button class="btn btn_blue">Diventa partner ></button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CollaboraConNoiComponent",
+};
+</script>
+
+<style lang="scss" scoped>
+@import "resources/sass/variables";
+
+#collab {
+  display: flex;
+  background-color: white;
+  .collab-cover {
+    background-image: url("/img/img rest/collab.jpg");
+    background-size: 120%;
+    background-repeat: no-repeat;
+    background-position: 90% 25%;
+    width: 60%;
+    min-height: 500px;
+  }
+  .collab-text {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: 35%;
+    text-align: end;
+    padding-right: 85px;
+    h2 {
+      font-size: 2.5rem;
+      font-weight: 600;
+      color: $primary-color;
+    }
+    p {
+      color: $primary-color;
+      margin-top: 2rem;
+      margin-bottom: 2rem;
+    }
+    button {
+      border-color: $primary-color;
+      &:hover {
+        background-color: $primary-color;
+      }
+    }
+  }
+}
+
+@media screen and (max-width: 767px) {
+  #collab {
+    flex-direction: column;
+    .collab-cover {
+      width: 100%;
+    }
+    .collab-text {
+      width: 100%;
+    }
+  }
+}
+</style>
---
+++ resources/js/components/home/ServiziComponent.vue
@@ -0,0 +1,91 @@
+<template>
+  <div id="services">
+    <div class="container">
+      <div class="section-title">
+        <h2>BOOLIVERY</h2>
+        <h3>Portiamo il cibo per tutta la city</h3>
+      </div>
+      <div class="services-list">
+        <div class="service">
+          <i class="fas fa-motorcycle"></i>
+          <span>CONSEGNA RAPIDA E GRATUITA</span>
+          <p>
+            La rapidità è un nostro punto d'orgoglio. Ordina o invia qualsiasi
+            cosa nella tua città e lo ritireremo e te lo consegneremo nel giro
+            di qualche minuto.
+          </p>
+        </div>
+        <div class="service">
+          <i class="fas fa-check"></i>
+          <span>SICUREZZA E CONTROLLO</span>
+          <p>
+            Abbiamo un team dedicato a seguire tutta la tua esperienza con
+            Boolivery, dal momento in cui effettui l'ordine a quando ti viene
+            consegnato.
+          </p>
+        </div>
+        <div class="service">
+          <i class="fas fa-money-bill-alt"></i>
+          <span>SODDISFATTI O RIMBORSATI</span>
+          <p>
+            Non sei soddisfatto del prodotto? Rimborseremo l'importo pagato
+            interamente. La vostra soddisfazione è alla base delle nostre
+            politiche di reso.
+          </p>
+        </div>
+        <div class="service">
+          <i class="fas fa-leaf"></i>
+          <span>ECO FRIENDLY</span>
+          <p>
+            Evitare la plastica si può, almeno dove è possibile. Noi di
+            Boolivery crediamo fermamente nei benefici del packaging ecologico.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ServiziComponent",
+};
+</script>
+
+<style lang="scss" scoped>
+@import "resources/sass/variables";
+
+#services {
+  background: $brand-color;
+  padding: 3rem;
+  .section-title {
+    text-align: center;
+    margin-bottom: 2rem;
+    h2 {
+      color: $primary-color;
+    }
+  }
+  .services-list {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    row-gap: 2rem;
+    .service {
+      display: flex;
+      flex-direction: column;
+      width: 35%;
+      text-align: center;
+    }
+  }
+}
+
+@media screen and (max-width: 630px) {
+  .services-list {
+    flex-direction: column;
+    align-items: center;
+    .service {
+      width: 100% !important;
+    }
+  }
+}
+</style>
