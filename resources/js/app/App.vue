d1f4255ddfa9eeb4e54825bf29841ec9e94dd44f
stefano472 <stefanopiotti472@gmail.com>
2022-06-28 10:45:34 +0200

    front-end init

--- resources/js/app/App.vue
+++ resources/js/app/App.vue
@@ -1,19 +1,34 @@
 <template>
   <div>
-    <!-- <HeaderComponent/> -->
+    <HeaderComponent/>
     <router-view/>
     <!-- oppure 
     <router-view></router-view> -->
+    <FooterComponent/>
   </div>
 </template>
 
 <script>
-// import HeaderComponent from '../components/HeaderComponent.vue'
+import HeaderComponent from '../components/HeaderComponent.vue';
+import FooterComponent from '../components/FooterComponent.vue';
+
 export default {
     name: 'App',
-    // components: { HeaderComponent },
+    components: { 
+        HeaderComponent,
+        FooterComponent,
+    },
 }
 </script>
 
-<style>
+<style lang="scss">
+*{
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+}
+ul{
+    list-style-type: none;
+    margin: 0;
+}
 </style>
---
+++ resources/js/components/HeaderComponent.vue
@@ -0,0 +1,84 @@
+<template>
+  <div id="header">
+    <div class="container">
+        <div class="logo">
+            <img src="logo.svg" alt="">
+            <p>oolivery</p>
+        </div>
+        <nav>
+            <ul> 
+                <li>
+                    <a href="">categorie</a>
+                </li>
+                <li>
+                    <a href="">collabora con noi</a>
+                </li>
+                <li>
+                    <a href="">about us</a>
+                </li>
+                <li>
+                    <a href="">contatti</a>
+                </li>
+            </ul>
+        </nav>
+        <div class="cart-login">
+            <div>
+                <a href="">accedi</a>
+            </div>
+            <div>
+                <button>cart</button>
+            </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'HeaderComponent',
+}
+</script>
+
+<style scoped lang="scss">
+@import 'resources/sass/variables';
+
+#header{
+    background: $dark-color;
+    .container{
+        color: $primary-color;
+        height: 70px;
+        max-width: 1400px;
+        margin-inline: auto;
+        display: flex;
+        justify-content: space-between;
+        padding: 0.5rem;
+        .logo{
+            display: flex;
+            img{
+                width: 100%;
+            }
+            p{
+                font-size: 2rem;
+                margin-top: 0.5rem;
+            }
+        }
+        nav{
+            display: flex;
+            align-items: center;
+            ul{
+                display: flex;
+                gap: 1rem;
+                li{
+                    a{
+                        color: $primary-color;
+                        cursor: pointer;
+                    }
+                }
+            }
+        }
+        .cart-login{
+            display: flex;
+        }
+    }
+}
+</style>
