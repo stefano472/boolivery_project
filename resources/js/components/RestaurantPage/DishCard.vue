b0843b9294531b783948d55aa5f41f88b0ab07b0
Alessandro Giunta <alegiunt99@gmail.com>
2022-06-30 17:21:51 +0100

    DishesList and DishCard components completed

---
+++ resources/js/components/RestaurantPage/DishCard.vue
@@ -0,0 +1,152 @@
+<template>
+
+  <div>
+
+    <!-- Card del piatto -->
+    <div class="dish-card" v-for="(dish, index) in dishes" :key="index">
+        
+        <!-- foto del piatto -->
+        <div class="foto" :id="{dish}">
+            <img src="../../../../public/img/img_rest/salad-with-jalapeno-1024x683.jpeg" alt="Dish">
+        </div>
+
+        <!-- nome e descrizione del piatto -->
+        <div class="name-description">
+
+            <!-- name -->
+            <div class="dish-name"> 
+                <h2>Ensalada de alapeno</h2>
+            </div>
+            
+            <!-- description -->
+            <div class="dish-description"> 
+                <p>Insalata di alapeno condita con oli aromatici, uva e salsa piccante</p>
+            </div>
+        </div>
+
+        <!-- prezzo e aggiungi al carrello -->
+        <div class="price add-to-cart">
+
+            <!-- prezzo -->
+            <div class="price-dish">
+                <p>
+                    &euro;12
+                </p>
+            </div>
+
+            <!-- pulsante di aggiunta al carrello -->
+            <div class="add-to-cart-btn">
+                <button class="btn btn-outline-info">
+                    Add to cart
+                </button>
+            </div>
+        </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'DishCard',
+
+    data(){
+        
+        return {
+
+            dishes: [ 1, 2, 3, 4, 5, 6 ],
+        }
+    }
+
+}
+</script>
+
+<style lang="scss" scoped>
+    .dish-card{
+        display: flex;
+        justify-content: space-around;
+        padding: 30px 20px;
+        border-bottom: 1px solid gray;
+        border-radius: 20px;
+        margin: 31px 0;
+
+        //foto del piatto
+        .foto{
+            width: 250px;
+            img{
+                width: 100%;
+                border-radius: 20px;
+                min-width: 146px;
+            }
+        }
+
+        // nome e descrizione del piatto
+        .name-description{
+            padding: 0 20px;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            row-gap: 20px;
+            .dish-name{
+                h2{
+                    color: #1e282f;
+                }
+            }
+            .dish-description{
+                p{
+                    color: #8c9e9e;
+                }
+            }
+        }
+
+        .price.add-to-cart {
+            display: flex;
+            flex-direction: column;
+            justify-content: space-around;
+            .price-dish{
+                font-size: 25px;
+            }
+        }
+    }
+
+     @media screen and (max-width: 484px){
+
+     .dish-card{
+        flex-wrap: wrap;
+        //foto del piatto
+        .foto{
+            width: 100%;
+            margin-bottom: 17px;
+            img{
+                width: 100%;
+                border-radius: 20px;
+            }
+        }
+
+        // nome e descrizione del piatto
+        .name-description{
+            width: 100%;
+            text-align: center;
+        }
+
+        .price.add-to-cart {
+            align-items: center;
+            
+            
+        }
+    }
+        
+ }
+
+
+ .btn-outline-info {
+                background-color: transparent;
+                color: #38A3A5;
+                border-color: #38A3A5;
+                &:hover{
+                    color: white;
+                    background-color: #38A3A5;
+                    border-color: #38A3A5;
+                }
+}
+</style>
--- resources/js/components/RestaurantPage/DishesListComponent.vue
+++ resources/js/components/RestaurantPage/DishesListComponent.vue
@@ -1,14 +1,37 @@
 <template>
-  <div>DishesListComponent</div>
+  <div class="dishes-list">
+
+    <div class="container">
+        <!-- I nostri piatti -->
+        <div class="title">
+            <h1>I NOSTRI PIATTI</h1>
+        </div>
+
+        <!-- lista dei piatti -->
+        <div>
+            <DishCard />
+        </div>
+    </div>
+    
+  </div>
 </template>
 
 <script>
+
+import DishCard from './DishCard.vue';
+
 export default {
-    name: 'DishesListComponent'
+    name: 'DishesListComponent',
+
+    components: {
+        DishCard
+    }
 
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+    h1{
+        text-align: center;
+    }
 </style>
