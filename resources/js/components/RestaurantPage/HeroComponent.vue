524c5295405c895c458053f8c38de9d4435cc2bc
larafenny <lara.filippone@gmail.com>
2022-06-30 18:17:53 +0200

    heroComponent.vue in RestaurantPage

--- resources/js/components/RestaurantPage/HeroComponent.vue
+++ resources/js/components/RestaurantPage/HeroComponent.vue
@@ -1,18 +1,81 @@
 <template>
-    <div >
-        ciaosxcascdszdvcsdvsdvdsvfdsfsdfafdasfad
+    <div class="container-hero">
+
+        <div class="info-resturant">
+            <h1>NOME RISTORANTE</h1>
+            <h2>MOTTO</h2>
+            <h3>Tipo di cucina</h3>
+            <button class="btn-go">Vai al ristorante</button>
+        </div>
     </div>
 
 </template>
 
 <script>
+
+
+
 export default {
     name: 'HeroComponent',
 
 }
+
 </script>
 
 <style lang="scss" scoped>
 
+    @import 'resources/sass/variables';
+
+
+    .container-hero{
+        display: flex;
+        justify-content: center;
+        background-image: url("/img/img_rest/jakub-kapusnak-4f4YZfDMLeU-unsplash-1024x683.jpeg");
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+        position: relative;
+        text-align: center;
+
+        .info-resturant{
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            background-color: $secondary-color;
+            opacity: 0.90;
+            padding: 50px 150px;
+            margin: 100px 0;
+            color: $dark-color;
+        }
+
+        h1{
+            color: $dark-color;
+            font-size: 46px;
+                }
+        h2{
+            font-size: 22px;
+        }
+        p{
+            color: $dark-color;
+            line-height: 3rem;
+            font-size: 16px;
+            padding-top: 20px
+        }
+
+        .btn-go{
+            width: max-content;
+            padding: 5px;
+            background-color: transparent;
+            cursor: pointer;
+            margin: 20px 0;
+        }
+    }
+
+    @media (max-width: 800px) {
+        .info-resturant{
+            width: 100%;
+            flex-direction: column;
+        }
+}
 
 </style>
